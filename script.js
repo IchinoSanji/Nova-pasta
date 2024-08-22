2afb7aec0964d53cb796002b5e24561443899636
Lucas <lucaseduardofeeh@gmail.com>
2024-08-22 19:43:41 -0300

    Atividade Feita

---
+++ script.js
@@ -0,0 +1,10 @@
+// Adiciona um efeito de rolagem suave para os links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
